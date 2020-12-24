0d125a355a4beec6a30b826a5148a195e7b438c1
inniyakrishnan <69983503+inniyakrishnan@users.noreply.github.com>
2020-12-24 14:06:23 -0800

    new activity

---
+++ app/src/main/java/com/mvhs/technovation/speechapp/Listening.java
@@ -0,0 +1,14 @@
+package com.mvhs.technovation.speechapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Listening extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listening);
+    }
+}
