8e09ed54c618921cf0ac7e732c85a8d55b698e49
amrita-arun <amrita.arun2013@gmail.com>
2020-12-24 17:12:58 -0800

    small arraylist of questions added, updated ui to include instructions and textview for sentence.

--- app/src/main/java/com/mvhs/technovation/speechapp/SpeakingActivity.java
+++ app/src/main/java/com/mvhs/technovation/speechapp/SpeakingActivity.java
@@ -13,17 +13,21 @@ import java.util.ArrayList;
 
 public class SpeakingActivity extends AppCompatActivity {
 
-//    private ArrayList<String> questions = new ArrayList<String>();
+    private ArrayList<String> questions = new ArrayList<String>();
 //    private int numQuestions;
-private static final int REQUEST_CODE = 100;
+    private static final int REQUEST_CODE = 100;
     private TextView textOutput;
+    private String spokenSentence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textOutput= (TextView) findViewById(R.id.textOutput);
-
+        questions.add("Today I went to the park and hung out with my friends.");
+        questions.add("Today was my birthday, I ate cake and pizza.");
+        questions.add("Reading is one of my favorite hobbies.");
+        questions.add("My favorite color is blue.");
     }
 
 //This method is called with the button is pressed//
@@ -56,6 +60,7 @@ private static final int REQUEST_CODE = 100;
                 if (resultCode == RESULT_OK && null != data) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     textOutput.setText(result.get(0));
+                    spokenSentence = result.get(0);
                 }
                 break;
             }
