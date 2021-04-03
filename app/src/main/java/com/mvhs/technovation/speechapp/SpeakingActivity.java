3d52cf357e625150dfda805daf34e35e9a89189e
amrita-arun <76417248+amrita-arun@users.noreply.github.com>
2021-04-03 16:20:51 -0700

    fixed compilation error after merge

--- app/src/main/java/com/mvhs/technovation/speechapp/SpeakingActivity.java
+++ app/src/main/java/com/mvhs/technovation/speechapp/SpeakingActivity.java
@@ -19,7 +19,6 @@ public class SpeakingActivity extends AppCompatActivity {
     private ArrayList<String> questions = new ArrayList<String>();
 //    private int numQuestions;
     private static final int REQUEST_CODE = 100;
-    private TextView textOutput;
     private String spokenSentence;
     private TextView sentence1;
     private TextView goodJob;
@@ -32,7 +31,6 @@ public class SpeakingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_speaking2);
-        textOutput= (TextView) findViewById(R.id.textOutput);
         questions.add("Today I went to the park and hung out with my friends");
         questions.add("Today was my birthday, I ate cake and pizza");
         questions.add("Reading is one of my favorite hobbies");
@@ -105,7 +103,6 @@ public class SpeakingActivity extends AppCompatActivity {
             case REQUEST_CODE: {
                 if (resultCode == RESULT_OK && null != data) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                 //   textOutput.setText(result.get(0));
                     spokenSentence = result.get(0);
                 }
                 break;
