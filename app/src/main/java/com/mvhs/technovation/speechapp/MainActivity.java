3dd0602757563f7179366a86802ba26ffab0fe9d
amrita-arun <amrita.arun2013@gmail.com>
2021-04-11 08:59:33 -0700

    goes from one screen to another, added google sign in auth

--- app/src/main/java/com/mvhs/technovation/speechapp/MainActivity.java
+++ app/src/main/java/com/mvhs/technovation/speechapp/MainActivity.java
@@ -6,14 +6,41 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button listening;
+    private Button realWorld;
+    private Button speaking;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //setContentView(R.layout.activity_listening);
+
+        listening = (Button)findViewById(R.id.listening);
+        realWorld = (Button)findViewById(R.id.realWorld);
+        speaking = (Button)findViewById(R.id.speaking);
+
+    }
+
+    public void onClickListening (View view)
+    {
         Intent intent = new Intent(this, Listening.class);
         startActivity(intent);
     }
+
+    public void onClickRealWorld (View view)
+    {
+        Intent intent = new Intent(this, RealWorld.class);
+        startActivity(intent);
+    }
+
+    public void onClickSpeaking (View view)
+    {
+        Intent intent = new Intent(this, SpeakingActivity.class);
+        startActivity(intent);
+    }
+
 }
