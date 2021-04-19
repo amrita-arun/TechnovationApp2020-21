ea596d32ef3432623f38ff18a67d3a302c61abbc
amrita-arun <amrita.arun2013@gmail.com>
2021-04-18 22:47:34 -0700

    fixed permissions check and some minor ui updates

--- app/src/main/java/com/mvhs/technovation/speechapp/MainActivity.java
+++ app/src/main/java/com/mvhs/technovation/speechapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.mvhs.technovation.speechapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 300;
     private Button listening;
     private Button realWorld;
     private Button speaking;
@@ -19,9 +26,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //setContentView(R.layout.activity_listening);
 
-        listening = (Button)findViewById(R.id.listening);
-        realWorld = (Button)findViewById(R.id.realWorld);
-        speaking = (Button)findViewById(R.id.speaking);
+        listening = findViewById(R.id.listening);
+        realWorld = findViewById(R.id.realWorld);
+        speaking = findViewById(R.id.speaking);
+
+        if (!arePermissionsGranted()) {
+            requestNeededPermissions();
+        }
+    }
+
+    private void requestNeededPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    private boolean arePermissionsGranted() {
+        int recordAudioPermResult = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+        int writeExternalStoragePermResult = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        return recordAudioPermResult == PackageManager.PERMISSION_GRANTED && writeExternalStoragePermResult == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode, String[] permissions, int[] grantResults){
+
+        switch(permsRequestCode){
+
+            case 300:
+
+                boolean recordAudioPermissionAccepted = grantResults[0]==PackageManager.PERMISSION_GRANTED;
+                boolean writeExtStoragePermissionAccepted = grantResults[1]==PackageManager.PERMISSION_GRANTED;
+
+                if (recordAudioPermissionAccepted && writeExtStoragePermissionAccepted) {
+                    listening.setEnabled(true);
+                    realWorld.setEnabled(true);
+                    speaking.setEnabled(true);
+                }
+                else {
+                    listening.setEnabled(false);
+                    realWorld.setEnabled(false);
+                    speaking.setEnabled(false);
+                    Toast.makeText(this, "Permissions need to be granted for features to work", Toast.LENGTH_LONG).show();
+                }
+                break;
+
+        }
 
     }
 
--- app/src/main/java/com/mvhs/technovation/speechapp/PongView.java
+++ app/src/main/java/com/mvhs/technovation/speechapp/PongView.java
@@ -224,9 +224,9 @@ class PongView extends SurfaceView implements Runnable {
             }
         }
         // Bounce the mBall back when it hits the top of screen
-        if(mBall.getRect().top < 30){
+        if(mBall.getRect().top < 100){
             mBall.reverseYVelocity();
-            mBall.clearObstacleY(100);
+            mBall.clearObstacleY(120);
 
             sp.play(beep2ID, 1, 1, 0, 0, 1);
         }
